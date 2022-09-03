23fdc26687e46d894cd771bcb2c71596392c6527
Krisztian Papp <fejlesztes@letscode.hu>
2022-09-03 10:50:59 +0200

    --amend

--- tests/InMemoryVehicleRepository.test.ts
+++ tests/InMemoryVehicleRepository.test.ts
@@ -1,13 +1,14 @@
 import {describe, beforeEach, expect, test} from '@jest/globals'
-import InMemoryVehicleRepository from '../src/Repositories/InMemoryVehicleRepository';
+import InMemoryVehicleRepository from '../src/Repositories/InMemoryVehicleRepository'
 
-describe('vehicle repository', () : void => {
-    describe('create', () : void => {
-        let underTest : InMemoryVehicleRepository;
+describe('vehicle repository', () => {
+
+    describe('create', () => {
+        let underTest: InMemoryVehicleRepository;
         beforeEach(() => {
             underTest = new InMemoryVehicleRepository();
         })
-        test('created vehicle is in the list', () : void => {
+        test('created vehicle is in the list', () => {
             underTest.add("car", "AAAA-001");
             const actual = underTest.list();
             expect(actual.length).toBe(1);
